96d03489c1f0f22f9eac84c189c983d717303b70
EndL11 <47588502+EndL11@users.noreply.github.com>
2020-05-19 09:08:10 +0300

    third program

---
+++ Source_3.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <stdio.h>
+void GetError() {
+	DWORD dw = GetLastError();
+	printf("GetLastError returned %u\n", dw);
+}
+void SetError() {
+	DWORD dw = ERROR_FILE_NOT_FOUND;
+	SetLastError(dw);
+}
+int main() {
+	SetError();
+	GetError();
+	system("pause");
+	return 0;
+}
