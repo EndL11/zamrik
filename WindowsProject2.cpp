3c15b44d86ac21911cd23da5afdd8a88de814daf
EndL11 <47588502+EndL11@users.noreply.github.com>
2020-05-19 09:38:40 +0300

    4 laboratory work

---
+++ WindowsProject2.cpp
@@ -0,0 +1,7 @@
+﻿#include <windows.h>;
+#include <tchar.h>;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(NULL, _T("Podobailo Andriy!\nHello, Win32 world!"), _T("Hello from Message Box"), MB_OK);
+	return 0;
+}
